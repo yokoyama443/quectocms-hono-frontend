6cd77ee88940095b1e09916d7443c0eb1b6649fc
yokoyama443 <j122357@ns.kogakuin.ac.jp>
2024-07-27 18:52:28 +0900

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ export default defineConfig({
   server: {
     proxy: {
       "/api": {
-        target: "http://localhost:8787/",
+        target: "https://api.cms.yokoyama443.dev/",
         changeOrigin: true
       }
     }
